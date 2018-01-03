9b5b65183bcec4a469ca65a0e8bb2a54c96beabf
Cássio Zen <cassiozen@gmail.com>
2018-01-03 04:57:15 -0500

    Bugfix: Instances of Date cannot have proxy. Closes #2

--- dist/index.js
+++ dist/index.js
@@ -49,24 +49,24 @@ const createEscapeWrapper = options => {
   const proxyHandler = {
     get: function(target, prop, receiver) {
       const value = Reflect.get(target, prop, receiver);
+      const type = typeof value;
 
-      switch (typeof value) {
-        case "object":
-          return new Proxy(value, proxyHandler);
-
-        case "function":
-          // The current value is a function: Is it in the functions whitelist?
-          const autoEscapedFn = options.fnWhitelist.find(
-            obj => obj === value || (obj[prop] && obj[prop] === value)
-          );
-          if (autoEscapedFn) {
-            return value;
-          } else {
-            return functionProxy(value);
-          }
-
-        default:
-          return escape(value);
+      if (value instanceof Date) {
+        return value;
+      } else if (type === "object") {
+        return new Proxy(value, proxyHandler);
+      } else if (type === "function") {
+        // The current value is a function: Is it in the functions whitelist?
+        const autoEscapedFn = options.fnWhitelist.find(
+          obj => obj === value || (obj[prop] && obj[prop] === value)
+        );
+        if (autoEscapedFn) {
+          return value;
+        } else {
+          return functionProxy(value);
+        }
+      } else {
+        return escape(value);
       }
     }
   };
